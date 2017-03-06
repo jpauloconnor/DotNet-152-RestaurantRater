125198413f30cd9c83cbdd8671366a88871c1c56
Paul O'Connor <jamespauloconnor@gmail.com>
2017-03-06 17:35:19 -0500

    Mod 5 done

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -8,10 +8,32 @@ namespace RestaurantRater.Controllers
     {
         private RestaurantDBContext db = new RestaurantDBContext();
 
-        // GET: Restaurants
+        // GET: Restaurant
         public ActionResult Index()
         {
             return View(db.Restaurants.ToList());
+        }
+
+        // GET: Restaurant/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        //POST: Restaurant/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "RestaurantID,Name")] Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Restaurants.Add(restaurant);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(restaurant);
         }
     }
 }
