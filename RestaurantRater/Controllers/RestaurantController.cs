864dc16095353f7bfc2b26e4f37afa3ae0a4ef3b
Paul O'Connor <jamespauloconnor@gmail.com>
2017-03-06 10:37:20 -0500

    add index view and controller method

---
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -0,0 +1,17 @@
+﻿using RestaurantRater.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace RestaurantRater.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        private RestaurantDBContext db = new RestaurantDBContext();
+
+        // GET: Restaurants
+        public ActionResult Index()
+        {
+            return View(db.Restaurants.ToList());
+        }
+    }
+}
